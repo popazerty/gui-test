47184ead8ed97e3f3fa8081d1477a34c6c4b49ec
opendroid-Team <opendroid2014@gmail.com>
2015-01-16 12:08:45 +0100

    [avswitch] Delete scart and ypbpr mode vuzero

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -71,7 +71,7 @@ class AVSwitch:
 		widescreen_modes = {"720p", "1080i"}
 
 	modes["YPbPr"] = modes["HDMI"]
-	if getBoxType().startswith('vu') or (getBoxType() in ('dm500hd', 'dm800')):
+	if getBrandOEM() == 'vuplus':
 		modes["Scart-YPbPr"] = modes["HDMI"]
 
 	# if modes.has_key("DVI-PC") and not getModeList("DVI-PC"):
